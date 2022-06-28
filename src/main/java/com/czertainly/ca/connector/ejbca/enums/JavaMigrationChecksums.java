2204f0bd488eb0f5f782c311a111d1230418429b
3KeyPradeep <pradeep.saminathan@3key.company>
2022-06-28 08:28:24 +0100

    Update checksum based on the latest code
    Update checksum based on the latest code

--- src/main/java/com/czertainly/ca/connector/ejbca/enums/JavaMigrationChecksums.java
+++ src/main/java/com/czertainly/ca/connector/ejbca/enums/JavaMigrationChecksums.java
@@ -4,7 +4,7 @@ package com.czertainly.ca.connector.ejbca.enums;
  * Stores the checksum of a Java-based migration.
  */
 public enum JavaMigrationChecksums {
-    V202206231700__AttributeChanges(-616266711);
+    V202206231700__AttributeChanges(1236550637);
     private final int checksum;
 
     JavaMigrationChecksums(int checksum) {
