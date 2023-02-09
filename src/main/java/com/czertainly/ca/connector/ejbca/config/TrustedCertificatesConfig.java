3dc515da094ef0306c6a74e4d696661e8ba65c05
Pradeep Saminathan <76426163+3KeyPradeep@users.noreply.github.com>
2023-02-09 20:33:58 +0530

    Fix sonarqube reported issues

--- src/main/java/com/czertainly/ca/connector/ejbca/config/TrustedCertificatesConfig.java
+++ src/main/java/com/czertainly/ca/connector/ejbca/config/TrustedCertificatesConfig.java
@@ -74,13 +74,17 @@ public class TrustedCertificatesConfig {
         String relativeCacertsPath = "/lib/security/cacerts".replace("/", File.separator);
         String filename = System.getProperty("java.home") + relativeCacertsPath;
         FileInputStream is = new FileInputStream(filename);
+        try {
 
-        logger.debug("Loading cacert in location: {}", filename);
-        KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-        String password = "changeit";
-        keystore.load(is, password.toCharArray());
+            logger.debug("Loading cacert in location: {}", filename);
+            KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+            String password = "changeit";
+            keystore.load(is, password.toCharArray());
 
-        return keystore;
+            return keystore;
+        } finally {
+            is.close();
+        }
     }
 
     private List<X509Certificate> getTrustedCertificates(String trustedCerts) {
