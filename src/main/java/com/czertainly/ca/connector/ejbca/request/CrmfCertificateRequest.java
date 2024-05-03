78b6295f48c55fe380972607f3aac05ada0d4c5e
3keyroman <roman.cinkais@3key.company>
2024-05-03 11:55:37 +0200

    Update CrmfCertificateRequest.java

--- src/main/java/com/czertainly/ca/connector/ejbca/request/CrmfCertificateRequest.java
+++ src/main/java/com/czertainly/ca/connector/ejbca/request/CrmfCertificateRequest.java
@@ -1,7 +1,7 @@
 package com.czertainly.ca.connector.ejbca.request;
 
 import com.czertainly.api.model.core.enums.CertificateRequestFormat;
-import com.czertainly.ca.connector.ejbca.exception.CertificateRequestException;
+import org.bouncycastle.asn1.crmf.CertReqMessages;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.cert.crmf.jcajce.JcaCertificateRequestMessage;
 
@@ -10,9 +10,11 @@ public class CrmfCertificateRequest implements CertificateRequest {
     private final byte[] encoded;
     private final JcaCertificateRequestMessage certificateRequestMessage;
 
-    public CrmfCertificateRequest(byte[] crmfRequest) throws CertificateRequestException {
+    public CrmfCertificateRequest(byte[] crmfRequest) {
         this.encoded = crmfRequest;
-        this.certificateRequestMessage = new JcaCertificateRequestMessage(crmfRequest);
+        CertReqMessages certReqMessages = CertReqMessages.getInstance(crmfRequest);
+        // we take only the first request
+        this.certificateRequestMessage = new JcaCertificateRequestMessage(certReqMessages.toCertReqMsgArray()[0]);
     }
 
     @Override
